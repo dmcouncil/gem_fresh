a3de1535de9041b157842a7e8d055c8a9909ba3e
Parker Morse <pmorse@dmgroupk12.com>
2017-04-05 15:19:38 -0400

    Catch the cases where the version string is nil

--- lib/gem_fresh/gem_version.rb
+++ lib/gem_fresh/gem_version.rb
@@ -6,8 +6,12 @@ module GemFresh
     # 7.6.2.11, 7.6, or even 7.6.2.beta. We just care about the first
     # three numbers
     def initialize(version_string)
-      @major, @minor, @patch = version_string.split('.').map(&:to_i)
-      @patch ||= 0
+      begin
+        @major, @minor, @patch = version_string.split('.').map(&:to_i)
+        @patch ||= 0
+      rescue NoMethodError
+        @major, @minor, @patch = [0,0,0]
+      end
     end
   end
 end
